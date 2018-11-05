058010406aa0d3d0a0df9efca5f028d1fce9d4dc
meng20181111 <jihuyi20160@sina.com>
2018-11-05 15:11:47 +0800

    Initial commit

---
+++ src/main/java/com/nmm/study/ConsumerApplication.java
@@ -0,0 +1,35 @@
+package com.nmm.study;
+
+import com.nmm.study.service.DubboServiceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@ImportResource("classpath:/dubbo.xml")
+@RestController
+public class ConsumerApplication {
+    /* 注入声明的服务 */
+    @Autowired
+    private DubboServiceProvider dubboServiceProvider1;
+
+    public  static void main(String[] args){
+        SpringApplication.run(ConsumerApplication.class,args);
+    }
+
+    //测试服务调用
+    @ResponseBody
+    @RequestMapping("/sayhello/{name}")
+    public String hello(@PathVariable String name){
+
+        System.out.println("-----name="+name);
+
+        return dubboServiceProvider1.sayHello(name);
+    }
+}
+
